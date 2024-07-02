96f2d9d76906181640571125c6dc6a52cea6472c
haiderali077 <haiderali077@users.noreply.github.com>
2024-07-02 03:35:54 -0700

    back end set up

---
+++ back end/routes/transactions.js
@@ -0,0 +1,16 @@
+const { addExpense, getExpense, deleteExpense } = require('../controllers/expense')
+const { addIncome, getIncome, deleteIncome } = require('../controllers/income')
+
+
+const router = require('express').Router()
+
+
+router.post('/add-income', addIncome)
+    .get('/get-incomes', getIncome)
+    .delete('/delete-income/:id', deleteIncome)
+    .post('/add-expenses', addExpense)
+    .get('/get-expenses', getExpense)
+    .delete('/delete-expense/:id', deleteExpense )
+
+
+module.exports = router
